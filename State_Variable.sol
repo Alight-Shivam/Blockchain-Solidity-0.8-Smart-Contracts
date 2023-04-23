c9bc801aae4db32479a19d37671d30d044b6a187
Alight-Shivam <97473543+Alight-Shivam@users.noreply.github.com>
2023-04-23 20:00:26 +0530

    Solidity

---
+++ State_Variable.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+// jo function ke to baahr define ho aur contract ke andar define ho
+
+contract stateVariable{
+    uint public salary;
+
+    constructor() {
+        salary = 1000;
+    }
+
+    function setSalary() public {
+        salary = 3000;
+    }
+
+}
+
+// state variables cost some amount of gas while storinng on bloackchain so we do not create large amount of state variablaes ,only required one are created
