7c462fc203c067749585dbf4c0256480cab781d3
Alight-Shivam <97473543+Alight-Shivam@users.noreply.github.com>
2023-04-28 23:48:53 +0530

    Solidity

--- Local_Variable.sol
+++ Local_Variable.sol
@@ -1,3 +1,23 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0; 
+
+contract localVariable {
+    unit public age;
+    bool public b;
+    address public newAdd;
+
+    function fun(uint _x, bool _y, address _z) public returns {
+        uint i = 29;
+        bool b1= true;
+
+        i+=29;
+        b1 = false;
+
+        age = _x;
+        b = _y;
+        newAdd=_z;
+
+        return (i,b1);
+    }
+}
